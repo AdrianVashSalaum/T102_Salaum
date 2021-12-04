0bb0a55b2dc085f0887d4dbc2c6a04dadac07ea8
AdrianVashSalaum <89187337+AdrianVashSalaum@users.noreply.github.com>
2021-12-04 13:57:24 +0800

    Add files via upload

---
+++ vash28.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(){
+
+ for(int a = 1; a < 10; a++){
+for(int v = a; v <= 9; v++){
+   printf("%d ", v);
+}
+printf("\n");
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
